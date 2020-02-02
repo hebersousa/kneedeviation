a67692f44916f7562cb18eee7bd87f6a8cab9ccb
heber <hebersousa@gmail.com>
2020-02-02 14:23:13 -0300

    saveimage

--- BasicAR/ImageSaver.swift
+++ BasicAR/ImageSaver.swift
@@ -16,12 +16,11 @@ class ImageSaver: NSObject {
      var errorHandler: ((Error) -> Void)?
     
     func writeToPhotoAlbum(image: UIImage) {
-        
         UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
     }
 
     @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        
+     
         if let error = error {
             errorHandler?(error)
         } else {
